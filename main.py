42dd33fc3e2ff03137230e200b77141277af45b5
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 15:59:23 -0700

    first push

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame 
+
+
+pygame.init()
+
+
+
+
+
+
+
+size = (700, 500)
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Dinar Dash")
+
+
+
+
+
+
+running = True 
+
+clock - pygame.time.Clock()
+
+
+while running:
+    
