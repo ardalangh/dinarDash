e7af75790681e1cfb9604514308b46ec479f8159
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 16:01:45 -0700

    writting the basic main loop

--- main.py
+++ main.py
@@ -25,4 +25,18 @@ clock - pygame.time.Clock()
 
 
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            running = False
+
+    
+
+
+
+
+
+
+
+    pygame.display.flip()
+    clock.tick(60)
     
