9d6712c218ae8ce2acef09bb3854edcd6f7bbbc6
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-04-02 16:29:37 -0700

    end of session

--- Table.py
+++ Table.py
@@ -1,4 +1,8 @@
-import pygame 
+import pygame
+
+from Chair import Chair
+
+
 class Table:
     possible_caps = [2, 4, 6, 8]
     possible_pos = [(400, 400),
@@ -7,31 +11,28 @@ class Table:
                     (400, 700)]
     file_path = "./assets/table.PNG"
 
-
-    def __init__(self, cap, id):
-        self.id = id
+    def __init__(self, cap, idNum):
+        self.id = idNum
         if cap not in Table.possible_caps:
             raise ValueError("The capacity of your table needs to in [2, 4, 6, 8]")
-    
-        self.cap = cap    # The cap of the table (int)
-        self.empty = True # Table is empty (bool)
-        self.guest_at = 0 # How many people are at this table (int)
-        self.num = None   # The number of the table (int)
-        self.chairs = []  # list of all the chairs around the instance of the table class
 
-
-
-
-
-
-
-
-
-
-
-
-
+        self.cap = cap      # The cap of the table (int)
+        self.empty = True   # Table is empty (bool)
+        self.guest_at = 0   # How many people are at this table (int)
+        self.num = None     # The number of the table (int)
+        self.chairs = []    # list of all the chairs around the instance of the table class
 
     def draw(self, screen):
+        for chair in self.chairs:
+            chair.draw(screen)
         table = pygame.image.load(Table.file_path).convert()
-        screen.blit(table, Table.possible_pos[self.id])
+        screen.blit(table, Table.possible_pos[self.idNum])
+
+    def initChairs(self):
+        self.chairs.appemnd(Chair(self.determineLeftChairPos()[0],
+                                  self.determineLeftChairPos()[1],
+                                  self,
+                                  0))
+
+    def determineLeftChairPos(self):
+        return self.x - 10, self.y
